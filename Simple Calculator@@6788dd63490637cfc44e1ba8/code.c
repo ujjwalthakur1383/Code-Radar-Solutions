35b4cbe0f9fbafd46ec9338e3e480dd4b485cbc6
ujjwalthakur1383 <ujjwal1383.becse24@chitkara.edu.in>
2025-02-12 03:12:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,5 +7,11 @@ int main(){
     if(c=='+'){
         printf("%d", a+b);
     }
+    else if (c=='-'){
+        printf("%d",a-b);
+    }
+    else if (c=='*'){
+        printf("%d",a*b);
+    }
     return 0;
 }
