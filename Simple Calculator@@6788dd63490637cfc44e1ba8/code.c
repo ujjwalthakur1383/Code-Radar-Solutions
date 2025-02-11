c41c60ff417c1d9f0fea2d36e7b64222710ef8e9
ujjwalthakur1383 <ujjwal1383.becse24@chitkara.edu.in>
2025-02-12 03:30:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,9 +4,8 @@ int main(){
     int a, b;
     char c;
 
-    // Read input for two integers and an operator
     scanf("%d %d", &a, &b);
-    scanf(" %c", &c); // Added space before %c to consume any lingering newline from the previous input
+    scanf(" %c", &c);
 
     if(c == '+'){
         printf("%d", a + b);
@@ -19,8 +18,8 @@ int main(){
     }
     else if(c == '/'){
         if(b != 0) {
-            float Quotient = a / b; // Make sure the division is done in double
-            printf("%1.lf", Quotient); 
+            float Quotient = a / b;
+            printf("%1.f", Quotient); 
         }
         else {
             printf("error");
