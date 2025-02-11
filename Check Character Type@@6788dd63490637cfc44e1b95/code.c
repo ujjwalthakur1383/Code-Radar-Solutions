70219c0025e97c8e5e9a6a9d97c54dc69fdae630
ujjwalthakur1383 <ujjwal1383.becse24@chitkara.edu.in>
2025-02-11 23:49:10 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,6 +6,9 @@ int main(){
     if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' ||a=='A' || a=='E' || a=='I' || a=='O' || a=='U'){
         printf("Vowel");
     }
+    else{
+        printf("Consonant");
+    }
 
     return 0;
 }
