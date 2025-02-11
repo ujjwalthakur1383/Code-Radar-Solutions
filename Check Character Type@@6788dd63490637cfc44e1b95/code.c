8c354cc1e7b212e94194e5ff043c390c563f584b
ujjwalthakur1383 <ujjwal1383.becse24@chitkara.edu.in>
2025-02-11 23:46:28 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char a;
+    int b;
+    scanf("%c %d",&a ,&b);
+    if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u'){
+        printf("Vowel");
+    }
+
+    return 0;
+}
